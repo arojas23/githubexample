3c50e3a6ad692af09c03eee754bf859eebdf5626
arojas23 <alvarok19@gmail.com>
2018-12-24 01:24:00 -0400

    Init Project

---
+++ GithubUsers/GithubUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUsers
+//
+//  Created by Alvaro Rojas  on 12/24/18.
+//  Copyright © 2018 Alvaro Rojas . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
