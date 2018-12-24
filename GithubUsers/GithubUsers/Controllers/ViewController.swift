cbcbf059339f365f40d91807f0f90ea246776baf
arojas23 <alvarok19@gmail.com>
2018-12-24 02:12:07 -0400

    Page 1 > Adding Custom Cells & UITable to UserListController

--- GithubUsers/GithubUsers/Controllers/ViewController.swift
+++ GithubUsers/GithubUsers/Controllers/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,25 @@ class ViewController: UIViewController {
         Alamofire.request("\(Constants.serverURL)/users").responseJSON { (response) -> Void in
             print(response)
         }
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a new cell if needed or reuse an old one
+        let cellReuseIdentifier = "cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
 
 
 }
---
+++ GithubUsers/GithubUsers/Views/UserCardCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCardCell.swift
+//  GithubUsers
+//
+//  Created by Alvaro Rojas  on 12/24/18.
+//  Copyright © 2018 Alvaro Rojas . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserCardCell: UITableViewCell {
+    
+    @IBOutlet weak var userProfileImageView: UIImageView!
+    @IBOutlet weak var userNameProfileLabel: UILabel!
+    @IBOutlet weak var userLinkProfileLabel: UILabel!
+    @IBOutlet weak var userReposProfileLabel: UILabel!
+}
