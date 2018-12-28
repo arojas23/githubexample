537f31fb8c367f3e5280bba06cd4429887762e2e
arojas23 <alvarok19@gmail.com>
2018-12-28 01:29:47 -0400

    Page 2 > Adding new Repo Cell

---
+++ GithubUsers/GithubUsers/Views/RepoCardCell.swift
@@ -0,0 +1,15 @@
+//
+//  RepoCardCell.swift
+//  GithubUsers
+//
+//  Created by Alvaro Rojas  on 12/28/18.
+//  Copyright © 2018 Alvaro Rojas . All rights reserved.
+//
+
+import UIKit
+
+class RepoCardCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
